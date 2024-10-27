76946b196955f709b8d67a456e652007beb439df
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-10-27 21:19:51 +0600

    Added navigation compose dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,12 +29,15 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            //Navigation with Compose
+            implementation(libs.navigation.compose)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
