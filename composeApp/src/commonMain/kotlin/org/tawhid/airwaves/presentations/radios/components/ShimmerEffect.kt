27e8c70f589003e9fc71911564db764b71249a4c
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-10-29 12:57:48 +0600

    Created shimmer effect for radio loading

--- composeApp/src/commonMain/kotlin/org/tawhid/airwaves/presentations/radios/components/ShimmerEffect.kt
+++ composeApp/src/commonMain/kotlin/org/tawhid/airwaves/presentations/radios/components/ShimmerEffect.kt
@@ -7,12 +7,99 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Brush
+import org.tawhid.airwaves.theme.imageSize
+import org.tawhid.airwaves.theme.mediumPadding
+import org.tawhid.airwaves.theme.xxLargePadding
+import org.tawhid.airwaves.theme.xxSmallPadding
+import org.tawhid.airwaves.theme.xxxLargePadding
+
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.TileMode
+import org.tawhid.airwaves.theme.cardMinSize
+import org.tawhid.airwaves.theme.shimmerColors
+
 
 @Composable
 fun ShimmerEffect() {
-    Box(
-        modifier = Modifier.fillMaxSize()
+
+    val transition = rememberInfiniteTransition()
+    val translateAnimation by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 400f,
+        animationSpec = infiniteRepeatable(
+            tween(durationMillis = 1500, easing = LinearOutSlowInEasing),
+            RepeatMode.Reverse
+        ),
+    )
+    val brush by remember {
+        derivedStateOf {
+            Brush.linearGradient(
+                colors = shimmerColors,
+                start = Offset(translateAnimation, translateAnimation),
+                end = Offset(translateAnimation + 100f, translateAnimation + 100f),
+                tileMode = TileMode.Mirror,
+            )
+        }
+    }
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Adaptive(cardMinSize),
+        verticalItemSpacing = mediumPadding,
+        horizontalArrangement = Arrangement.spacedBy(mediumPadding),
+        contentPadding = PaddingValues(mediumPadding),
+        userScrollEnabled = false
     ) {
-        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        repeat(30) {
+            item {
+                RadioShimmerEffect(brush)
+            }
+        }
+    }
+}
+
+@Composable
+fun RadioShimmerEffect(brush: Brush) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(xxSmallPadding)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(imageSize)
+                .background(brush, RoundedCornerShape(10))
+        )
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(xxSmallPadding)
+
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(xxxLargePadding)
+                    .background(brush, RoundedCornerShape(10))
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(xxLargePadding)
+                    .background(brush, RoundedCornerShape(10))
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.4f)
+                    .height(mediumPadding)
+                    .background(brush, RoundedCornerShape(10))
+            )
+        }
     }
 }
