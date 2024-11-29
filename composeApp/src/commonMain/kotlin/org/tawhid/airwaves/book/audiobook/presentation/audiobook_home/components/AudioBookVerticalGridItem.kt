a5f62b782561e024f14f8a1808c9e69e0b0e0eab
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-11-30 04:55:59 +0600

    AudioBooks UI Updated

--- composeApp/src/commonMain/kotlin/org/tawhid/airwaves/book/audiobook/presentation/audiobook_home/components/AudioBookVerticalGridItem.kt
+++ composeApp/src/commonMain/kotlin/org/tawhid/airwaves/book/audiobook/presentation/audiobook_home/components/AudioBookVerticalGridItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,6 +27,7 @@ import coil3.compose.AsyncImage
 import org.tawhid.airwaves.book.audiobook.domain.AudioBook
 import org.tawhid.airwaves.core.theme.Shapes
 import org.tawhid.airwaves.core.theme.cardMinSize
+import org.tawhid.airwaves.core.theme.imageSize
 import org.tawhid.airwaves.core.theme.xSmallPadding
 import org.tawhid.airwaves.core.theme.xxSmallPadding
 import org.tawhid.airwaves.core.theme.xxxSmallPadding
@@ -38,6 +40,8 @@ fun AudioBookVerticalGridItem(
 ) {
     Surface(
         modifier = modifier
+            .width(imageSize)
+            .height(cardMinSize * 1.4f)
             .padding(xxxSmallPadding)
             .clip(Shapes.small)
             .clickable(onClick = onClick)
@@ -53,53 +57,40 @@ fun AudioBookVerticalGridItem(
                 .height(IntrinsicSize.Min),
             horizontalAlignment = Alignment.Start
         ) {
-            audioBook.imgUrl?.let { BookImage(it) }
+            audioBook.imgUrl?.let {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(0.65f)
+                        .clip(Shapes.small)
+                ) {
+                    AsyncImage(
+                        model = audioBook.imgUrl,
+                        //placeholder = painterResource(id = R.drawable.placeholder),
+                        //error = painterResource(id = R.drawable.error_image),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
             Spacer(modifier = Modifier.height(xxSmallPadding))
-            BookTitle(audioBook.title)
+            Text(
+                text = audioBook.title,
+                style = MaterialTheme.typography.titleSmall,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.fillMaxWidth()
+            )
             audioBook.authors.firstOrNull()?.let { authorName ->
-                BookAuthor(authorName)
+                Text(
+                    text = authorName,
+                    style = MaterialTheme.typography.bodySmall,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
-}
-
-@Composable
-fun BookImage(imgUrl: String) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(0.65f)
-            .clip(Shapes.small)
-    ) {
-        AsyncImage(
-            model = imgUrl,
-            //placeholder = painterResource(id = R.drawable.placeholder),
-            //error = painterResource(id = R.drawable.error_image),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
-    }
-}
-
-@Composable
-fun BookTitle(title: String) {
-    Text(
-        text = title,
-        style = MaterialTheme.typography.titleSmall,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        modifier = Modifier.fillMaxWidth()
-    )
-}
-
-@Composable
-fun BookAuthor(authorName: String) {
-    Text(
-        text = authorName,
-        style = MaterialTheme.typography.bodySmall,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        modifier = Modifier.fillMaxWidth()
-    )
 }
