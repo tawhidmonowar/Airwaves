5613c1e95c3b5bdf95312aae64904ff3fc0f0bff
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-11-30 04:54:57 +0600

    Theme Files Created in Core

---
+++ composeApp/src/commonMain/kotlin/org/tawhid/airwaves/core/theme/Dimen.kt
@@ -0,0 +1,31 @@
+package org.tawhid.airwaves.core.theme
+
+import androidx.compose.ui.unit.dp
+
+val maxWidthIn = 1000.dp
+val staggeredGridCellSmall = 100.dp
+val staggeredGridCellLarge = 160.dp
+
+
+
+
+
+val xxxSmallPadding = 2.dp
+val xxSmallPadding = 5.dp
+val xSmallPadding = 8.dp
+val smallPadding = 12.dp
+val mediumPadding = 16.dp
+val largePadding = 20.dp
+val xLargePadding = 24.dp
+val xxLargePadding = 32.dp
+val xxxLargePadding = 48.dp
+
+val cardMinSizeSmall = 100.dp
+val cardMinSize = 160.dp
+val cardMinSizeLarge = 250.dp
+
+val imageSize = 100.dp
+val imageSizeMedium = 200.dp
+val imageSizeLarge = 250.dp
+
+
---
+++ composeApp/src/commonMain/kotlin/org/tawhid/airwaves/core/theme/Shape.kt
@@ -0,0 +1,12 @@
+package org.tawhid.airwaves.core.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(2.dp),
+    small = RoundedCornerShape(5.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(20.dp)
+)
