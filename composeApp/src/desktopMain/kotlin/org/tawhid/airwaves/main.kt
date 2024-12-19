d69491bd316ad4ce9fef162376b6ff76851c0c46
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-12-19 20:17:15 +0600

    Desktop window default size updated
    Desktop window default size and minimum window size updated

--- composeApp/src/desktopMain/kotlin/org/tawhid/airwaves/main.kt
+++ composeApp/src/desktopMain/kotlin/org/tawhid/airwaves/main.kt
@@ -1,12 +1,20 @@
 package org.tawhid.airwaves
 
+import airwaves.composeapp.generated.resources.Res
+import airwaves.composeapp.generated.resources.app_name
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.resources.stringResource
 import org.tawhid.airwaves.app.App
-import org.tawhid.airwaves.di.initKoin
+import org.tawhid.airwaves.core.di.initKoin
+import org.tawhid.airwaves.core.theme.defaultWindowHeight
+import org.tawhid.airwaves.core.theme.defaultWindowWidth
+import org.tawhid.airwaves.core.theme.minWindowHeight
+import org.tawhid.airwaves.core.theme.minWindowWidth
 import java.awt.Dimension
 
 fun main() {
@@ -15,11 +23,12 @@ fun main() {
         Window(
             onCloseRequest = ::exitApplication,
             state = WindowState(
-                position = WindowPosition(Alignment.Center)
+                position = WindowPosition(Alignment.Center),
+                size = DpSize(defaultWindowWidth, defaultWindowHeight),
             ),
-            title = "Airwaves",
+            title = stringResource(Res.string.app_name),
         ) {
-            window.minimumSize = Dimension(640, 480)
+            window.minimumSize = Dimension(minWindowWidth, minWindowHeight)
             App()
         }
     }
